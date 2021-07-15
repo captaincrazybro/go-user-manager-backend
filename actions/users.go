5d2522ac3df1cb4071d2dfbe1143e8f026ec349f
cgordon28 <camerongordon111@gmail.com>
2021-07-15 15:30:06 -0400

    working on making creating users possible

---
+++ actions/users.go
@@ -0,0 +1,21 @@
+package actions
+
+import (
+    "net/http"
+
+    "github.com/cgordon28/coke/models"
+	"github.com/gobuffalo/buffalo"
+)
+
+func ListUsers(c buffalo.Context) error {
+    users := models.Users{}
+    err := models.DB.All(&users)
+    if err != nil {
+        return c.Render(http.StatusOK, r.JSON(err))
+    }
+    return c.Render(http.StatusOK, r.JSON(users))
+}
+
+//func CreateUser(c buffalo.Context) error {
+
+//}
---
+++ models/go.mod
@@ -0,0 +1,14 @@
+module coke/models
+
+go 1.16
+
+require (
+	github.com/gobuffalo/buffalo v0.16.26 // indirect
+	github.com/gobuffalo/envy v1.9.0
+	github.com/gobuffalo/mw-csrf v1.0.0 // indirect
+	github.com/gobuffalo/packr/v2 v2.8.1
+	github.com/gobuffalo/pop/v5 v5.3.4
+	github.com/gobuffalo/suite v2.8.2+incompatible
+	github.com/gobuffalo/validate/v3 v3.3.0
+	github.com/gofrs/uuid v4.0.0+incompatible
+)
